5ae6273937ea3165bcabb89fd8c5416defcc98ef
Parker Sipes <parker@parkersipes.com>
2019-03-06 11:17:06 -0500

    pushing to github

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#annilistapp").attr({
+    target: "_blank",
+    href: "/assets/annilist.apk"
+  });
+
+  $("#mobilock").attr({
+    target: "_blank",
+    href: "/assets/mobilock.apk"
+  });
+
+  $("#bottledater").attr({
+    target: "_blank",
+    href: "/assets/bottleDate.apk"
+  });
+
+  $("#bottleread").attr({
+    target: "_blank",
+    href: "/assets/bottleRead.apk"
+  });
+});
